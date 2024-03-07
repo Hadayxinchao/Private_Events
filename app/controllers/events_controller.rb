eeb0d93cf99f2b5ae2d6fa372d6bd0570709721c
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-08 00:38:42 +0700

    Setup User Devise

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: :index
   before_action :set_event, only: %i[ show edit update destroy ]
 
   # GET /events or /events.json
